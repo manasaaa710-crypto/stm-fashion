8a84b0a27e1b7d9dd022aa57c1fd8adf7bd3fd66
Manasa Vukkadala <vukkadalamanasa@gmail.com>
2026-06-25 17:40:03 +0530

    Updated STM Studio Hero Section

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 // HERO SLIDER
-
-const slides = document.querySelectorAll(".hero-slide");
-
-if (slides.length > 0) {
-
-    let currentSlide = 0;
-
-    function showSlide(index) {
-
-        slides.forEach(slide => {
-            slide.classList.remove("active");
-        });
-
-        slides[index].classList.add("active");
+(function() {
+    const heroSlides = document.querySelectorAll(".slide, .hero-slide");
+    if (heroSlides.length > 0) {
+        let currentSlide = 0;
+        function showSlide(index) {
+            heroSlides.forEach(slide => {
+                slide.classList.remove("active");
+            });
+            heroSlides[index].classList.add("active");
+        }
+        setInterval(() => {
+            currentSlide = (currentSlide + 1) % heroSlides.length;
+            showSlide(currentSlide);
+        }, 4000);
     }
-
-    setInterval(() => {
-
-        currentSlide = (currentSlide + 1) % slides.length;
-
-        showSlide(currentSlide);
-
-    }, 4000);
-}
-
-setInterval(() => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}, 4000);
+})();
 // Cart
 
 let cartCount = JSON.parse(localStorage.getItem("cart"))?.length || 0;
@@ -142,7 +129,7 @@ product.querySelector("h3").innerHTML.toLowerCase();
 if(name.includes(input)){
 
 
-product.style.display="block";
+product.style.display="";
 
 
 }
@@ -159,3 +146,4 @@ product.style.display="none";
 
 
 }
+// End of script
