f8d36e9abeb6e0e246266281ada9b0baa4eeef49
Manasa Vukkadala <vukkadalamanasa@gmail.com>
2026-06-25 10:10:42 +0530

    STM Studio login page added

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
-const slides = document.querySelectorAll(".hero-slide");
-let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
 
 function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
     slides[index].classList.add("active");
 }
 
 setInterval(() => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+
+    current++;
+
+    if (current >= slides.length) {
+        current = 0;
+    }
+
+    showSlide(current);
+
 }, 4000);
+function togglePassword() {
+
+    let password = document.getElementById("password");
+
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+
+}
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+setInterval(() => {
+
+    current++;
+
+    if (current >= slides.length) {
+        current = 0;
+    }
+
+    showSlide(current);
+
+}, 4000);
+
+
+function togglePassword() {
+
+    let password = document.getElementById("password");
+
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+
+}
